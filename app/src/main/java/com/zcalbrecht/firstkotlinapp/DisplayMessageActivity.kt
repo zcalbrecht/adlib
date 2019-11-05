ec2c0b09a89f3dcf715ecbdd5eec47269abb7168
zcalbrecht <zcalbrecht@gmail.com>
2019-11-04 23:08:44 -0600

    Finalized interface & added toast

--- app/src/main/java/com/zcalbrecht/firstkotlinapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/zcalbrecht/firstkotlinapp/DisplayMessageActivity.kt
@@ -2,7 +2,6 @@ package com.zcalbrecht.firstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
@@ -12,36 +11,43 @@ class DisplayMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_display_message)
 
         // Get the Intent that started this activity and extract the string
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val word1 = intent.getStringExtra(WORD_1)
+        val word2 = intent.getStringExtra(WORD_2)
+        val word3 = intent.getStringExtra(WORD_3)
+        val word4 = intent.getStringExtra(WORD_4)
+        val word5 = intent.getStringExtra(WORD_5)
+        val word6 = intent.getStringExtra(WORD_6)
+        val word7 = intent.getStringExtra(WORD_7)
+        val word8 = intent.getStringExtra(WORD_8)
 
         // Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
+        val answer1 = findViewById<TextView>(R.id.answer1).apply {
+            text = word1
+        }
+
+        val answer2 = findViewById<TextView>(R.id.answer2).apply {
+            text = word2
+        }
+
+        val answer3 = findViewById<TextView>(R.id.answer3).apply {
+            text = word3
+        }
+
+        val answer4 = findViewById<TextView>(R.id.answer4).apply {
+            text = word4
+        }
+
+        val answer5 = findViewById<TextView>(R.id.answer5).apply {
+            text = word5
+        }
+
+        val answer6 = findViewById<TextView>(R.id.answer5).apply {
+            text = word6
+        }
+
+        val answer7 = findViewById<TextView>(R.id.answer6).apply {
+            text = word7
         }
     }
 
-    fun counter (view: View) {
-
-        val showCountTextView = findViewById<TextView>(R.id.textView)
-        val countString = showCountTextView.text.toString()
-
-        var count = 0
-
-        try {
-            count = Integer.parseInt(countString)
-        }
-        catch (e: NumberFormatException) {
-            count = 0
-        }
-
-        count++
-
-        showCountTextView.text = count.toString()
-    }
-
-    fun random (view: View) {
-        val showCountTextView = findViewById<TextView>(R.id.textView)
-        val randomInt = (0..10).random()
-        showCountTextView.text = randomInt.toString()
-    }
 }
--- app/src/main/java/com/zcalbrecht/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/zcalbrecht/firstkotlinapp/MainActivity.kt
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 
-const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
+const val WORD_1 = "com.example.myfirstapp.MESSAGE1"
+const val WORD_2 = "com.example.myfirstapp.MESSAGE2"
+const val WORD_3 = "com.example.myfirstapp.MESSAGE3"
+const val WORD_4 = "com.example.myfirstapp.MESSAGE4"
+const val WORD_5 = "com.example.myfirstapp.MESSAGE5"
+const val WORD_6 = "com.example.myfirstapp.MESSAGE6"
+const val WORD_7 = "com.example.myfirstapp.MESSAGE7"
+const val WORD_8 = "com.example.myfirstapp.MESSAGE8"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +24,44 @@ class MainActivity : AppCompatActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
+
+        val editText1 = findViewById<EditText>(R.id.editText1)
+        val editText2 = findViewById<EditText>(R.id.editText2)
+        val editText3 = findViewById<EditText>(R.id.editText3)
+        val editText4 = findViewById<EditText>(R.id.editText4)
+        val editText5 = findViewById<EditText>(R.id.editText5)
+        val editText6 = findViewById<EditText>(R.id.editText6)
+        val editText7 = findViewById<EditText>(R.id.editText7)
+        val editText8 = findViewById<EditText>(R.id.editText8)
+
+        val message1 = editText1.text.toString()
+        val message2 = editText2.text.toString()
+        val message3 = editText3.text.toString()
+        val message4 = editText4.text.toString()
+        val message5 = editText5.text.toString()
+        val message6 = editText6.text.toString()
+        val message7 = editText7.text.toString()
+        val message8 = editText8.text.toString()
+        if( message1 != "" && message2 != "" && message3 != "" && message4 != "" && message5 != "" && message6 != "" && message7 != "" && message8 != "" ){
+            val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+                putExtra(WORD_1, message1)
+                putExtra(WORD_2, message2)
+                putExtra(WORD_3, message3)
+                putExtra(WORD_4, message4)
+                putExtra(WORD_5, message5)
+                putExtra(WORD_6, message6)
+                putExtra(WORD_7, message7)
+                putExtra(WORD_8, message8)
+            }
+            startActivity(intent)
         }
-        startActivity(intent)
+        else
+        {
+            val text = "You didn't complete all the fields!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
     }
 }
